45c5f38603e37abb35a192b9e7da22c8693d9d5f
Kazakoff <atleast24.7@list.ru>
2022-02-10 20:35:45 +0800

    ❍ Automatic commit by: Kazakoff
    ☐--------------------------------------------------------☐
      MODIFIED:  babel.config.js
      MODIFIED:  package.json
      MODIFIED:  plugins/plugin-chart-echarts/package.json
      MODIFIED:  plugins/plugin-chart-echarts/tsconfig.json
      MODIFIED:  yarn.lock
    ☐--------------------------------------------------------☐
    Generated: Feb 10th [ 20:35:00 ]

--- babel.config.js
+++ babel.config.js
@@ -19,6 +19,7 @@ config.plugins = [
   ['babel-plugin-transform-dev', { evaluate: false }],
   ['babel-plugin-typescript-to-proptypes', { loose: true }],
   ['@babel/plugin-proposal-class-properties', { loose: true }],
+  ['@babel/plugin-proposal-optional-chaining', { loose: true }],
 ];
 config.presets.push([
   '@emotion/babel-preset-css-prop',
